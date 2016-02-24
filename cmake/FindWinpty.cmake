4c7b07b35e13d0ed748610fbc3aea576817aa2f6
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-02-24 20:05:50 +0000

    Fix FindWinpty.cmake
    Add find_package_handle_standard_args() to check result. Use find_program()
    for DLLs and find library for lib. Also added a condition for MSVC vs MinGW
    but it has yet to be tested.

--- cmake/FindWinpty.cmake
+++ cmake/FindWinpty.cmake
@@ -1,6 +1,18 @@
-find_path(WINPTY_INCLUDE_DIRS   winpty.h            ${WINPTY_CHECKOUT_PATH}/src/include)
-find_file(WINPTY_DLL            winpty.dll          ${WINPTY_CHECKOUT_PATH}/build)
-find_file(WINPTY_LIB            winpty.lib          ${WINPTY_CHECKOUT_PATH}/build)
-find_file(WINPTY_AGENT_EXE      winpty-agent.exe    ${WINPTY_CHECKOUT_PATH}/build)
-set(WINPTY_LIBRARIES            ${WINPTY_LIB})
-set(WINPTY_BINARIES             ${WINPTY_DLL} ${WINPTY_AGENT_EXE})
+
+include(LibFindMacros)
+
+find_path(WINPTY_INCLUDE_DIR winpty.h)
+set(WINPTY_INCLUDE_DIRS ${WINPTY_INCLUDE_DIR})
+
+find_library(WINPTY_LIBRARY winpty)
+find_program(WINPTY_AGENT_EXE winpty-agent.exe)
+set(WINPTY_LIBRARIES ${WINPTY_LIBRARY})
+
+if(MSVC)
+  find_program(WINPTY_DLL winpty.dll)
+  set(WINPTY_BINARIES ${WINPTY_DLL} ${WINPTY_AGENT_EXE})
+else()
+  set(WINPTY_BINARIES ${WINPTY_AGENT_EXE})
+endif()
+
+find_package_handle_standard_args(Winpty DEFAULT_MSG WINPTY_LIBRARY WINPTY_INCLUDE_DIR)
